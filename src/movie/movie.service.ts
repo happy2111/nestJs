d33b2e06f8cd8fa0b1aa75e8e887ed0878ae2a74
Ping <muhammadyusufa2111@gmail.com>
2025-10-18 12:25:30 +0500

    Initialize project with NestJS framework setup.
    - Added `.gitignore` with standard Node.js and development files.
    - Configured Prettier and ESLint for consistent coding standards.
    - Created `AppModule`, `AppController`, and `AppService` to handle basic functionality.
    - Integrated Movie and User modules.
    - Added End-to-End test setup with Jest.
    - Configured `package.json` and dependencies for NestJS development.

---
+++ src/movie/movie.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import {UserService} from "../user/user.service";
+
+@Injectable()
+export class MovieService {
+  constructor(private readonly userService: UserService) {
+
+  }
+  async test() {
+    return this.userService.test()
+  }
+}
